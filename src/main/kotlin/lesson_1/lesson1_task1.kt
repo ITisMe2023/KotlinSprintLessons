3c8696eec4b4343361f2e9dfda6ff70588864671
anato <tolik.nikulin98@mail.ru>
2025-02-08 20:54:17 +0300

    KS-1-1 Add variables

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,6 +1,8 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Тестовое решение к уроку 1 задача 1")
-    println("Новые доработки")
+
+    var numberOfOrders: Int = 42
+    var thanksText: String = "Спасибо"
+
 }
