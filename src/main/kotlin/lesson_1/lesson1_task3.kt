fe265dacdfa2303fbe1123d8233ae99c010d3333
anato <tolik.nikulin98@mail.ru>
2025-02-09 13:06:31 +0300

    KS-1-3 Space Flight

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,17 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Тестовое решение к уроку 1 задача 3")
+    val year: Int = 1961
+
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("Год полета: $year\n" +
+            "Час полета: $hour\n" +
+            "Минута полета: $minute\n")
+
+    hour = 10
+    minute = 55
+
+    println("Время посадки: $hour:$minute")
 }
